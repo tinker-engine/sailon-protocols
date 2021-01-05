7776c5d3d2ab7d2c1aa67f547635ec63c213968e
Najam Syed <najam.syed@kitware.com>
2021-01-05 13:03:46 -0500

    Create a simple default config and update it in the protocol constructor

--- ond/ond.py
+++ ond/ond.py
@@ -1,22 +1,10 @@
 import json
 import pathlib
 
+from config import default_config
+
 # TODO: this import is a placeholder
-import tinker_engine
-
-
-def default_config():
-    config = {
-        "detector_config": {
-            "feature_extractor_params": {
-                "backbone_weight_path": "",
-                "name": "i3d",
-                "arch": "i3d-50",
-                # TODO
-            },
-        },
-    }
-    return config
+#import tinker_engine
 
 
 class ONDProtocol():
@@ -30,29 +18,27 @@ class ONDProtocol():
             This will be updated alongside development of tinker engine
             config handling.
         """
+        self.config = default_config()
+
         if isinstance(config, str):
             config_fpath = pathlib.Path(config).expanduser().resolve()
             if not config_fpath.exists():
                 raise FileNotFoundError(f"File {config_fpath} does not exist")
             
             with open(config_fpath, "r") as f:
-                self.config = json.load(f)
-        elif isinstance(config, dict):
-            self.config = config
-        else:
-            raise ValueError("config must be dict or path to config file")
+                config = json.load(f)
 
-        # TODO: get default config from `default_config` and update it with
-        # the options present in `config`?
+        if config is not None:
+            self.config.update(config)
 
         # Get algorithm name from config.
         # TODO: rename this field in the config?
         novelty_algorithm_name = self.config["novelty_detector_class"]
 
-        # TODO: import/load the algorithm class via tinker/smqtk?
+        # TODO: This algorithm will ultimately be obtained via smqtk.
         # TODO: rename "detector_config" to "algorithm_config"?
-        NoveltyAlgorithm = tinker_engine.get_algorithm(novelty_algorithm_name)
-        self.algorithm = NoveltyAlgorithm(self.config["detector_config"])
+        #NoveltyAlgorithm = tinker_engine.get_algorithm(novelty_algorithm_name)
+        #self.algorithm = NoveltyAlgorithm(self.config["detector_config"])
 
         # TODO: get/instantiate interface from config? Or pass instance to
         # constructor?
@@ -67,11 +53,11 @@ class ONDProtocol():
             # NOTE: Use test_params on a per-test basis instead of toolset?
             test_params = self.interface.get_test_metadata(sess_id, test_id)
 
-            # TODO: better way to handle save_attributes.
+            # TODO: Handle save_attributes more robustly.
             if self.config["save_attributes"]:
                 test_params["attributes"] = {}
 
-            # TODO: Have a boolean "red_light" field?
+            # TODO: Use a boolean "red_light" field?
             if "red_light" in test_params["metadata"]:
                 pass
             else:
@@ -95,3 +81,6 @@ class ONDProtocol():
                 test_params["image_classification_feedback"] = None
 
             # TODO: remainder of test functionality.
+
+if __name__ == "__main__":
+    protcol = ONDProtocol(None, None)
