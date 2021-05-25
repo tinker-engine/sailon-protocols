9701b938a37cccd81cad8c1362e4a861470f8602
Najam Syed <najam.syed@kitware.com>
2021-05-25 14:55:24 -0400

    Add novelty interface abstract class

---
+++ sailon/novelty_interface.py
@@ -0,0 +1,31 @@
+import abc
+from typing import Tuple
+
+#from smqtk_core import Plugfigurable
+from smqtk_core import Configurable, Pluggable
+
+
+class ONDAdapter(Configurable, Pluggable, abc.ABC):
+    @abc.abstractmethod
+    def get_config(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def feature_extraction(self, test_params: dict) -> Tuple[dict, dict]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def world_detection(self, test_params: dict, test_data: dict) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def novelty_adaption(self, test_params: dict, test_data: dict) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def novelty_characterization(self, test_params: dict, test_data: dict) -> str:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def novelty_classification(self, test_params: dict, test_data: dict) -> str:
+        raise NotImplementedError
