7639b46ccface6acc6bc82fc2ab3e68e8a1ac121
Najam Syed <najam.syed@kitware.com>
2021-05-27 13:52:05 -0400

    Use pass instead of NotImplementedError

--- sailon/ond_adapter.py
+++ sailon/ond_adapter.py
@@ -8,7 +8,7 @@ from smqtk_core import Configurable, Pluggable
 class ONDAdapter(Configurable, Pluggable, abc.ABC):
     @abc.abstractmethod
     def get_config(self):
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def initialize(self, config_params: dict) -> None:
@@ -16,20 +16,20 @@ class ONDAdapter(Configurable, Pluggable, abc.ABC):
 
     @abc.abstractmethod
     def feature_extraction(self, test_params: dict) -> Tuple[dict, dict]:
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def world_detection(self, test_params: dict, test_data: dict) -> str:
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def novelty_adaption(self, test_params: dict, test_data: dict) -> None:
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def novelty_characterization(self, test_params: dict, test_data: dict) -> str:
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def novelty_classification(self, test_params: dict, test_data: dict) -> str:
-        raise NotImplementedError
+        pass
