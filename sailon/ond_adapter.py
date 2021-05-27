06c00df30153d090d51212b2f3a0ea0b38db9395
Najam Syed <najam.syed@kitware.com>
2021-05-27 14:05:07 -0400

    Add docstrings to abstract methods

--- sailon/ond_adapter.py
+++ sailon/ond_adapter.py
@@ -1,35 +1,93 @@
 import abc
 from typing import Tuple
 
-#from smqtk_core import Plugfigurable
 from smqtk_core import Configurable, Pluggable
 
 
 class ONDAdapter(Configurable, Pluggable, abc.ABC):
+    """
+    Abstract interface class for algorithm/algorithm adapters to be run with
+    the OND protocol.
+    """
+
     @abc.abstractmethod
     def get_config(self):
-        pass
+        """
+        :meth:`get_config` method required by SMQTK.
+        """
 
     @abc.abstractmethod
     def initialize(self, config_params: dict) -> None:
-        pass
+        """
+        Algorithm initialization step.
+
+        Args:
+            config_params: Dict containing configuration parameters for
+                algorithm initialization.
+        """
 
     @abc.abstractmethod
     def feature_extraction(self, test_params: dict) -> Tuple[dict, dict]:
-        pass
+        """
+        Algorithm feature extraction step.
+
+        Args:
+            test_params: Dict containing test parameters (e.g., input
+                filepaths) for the algorithm feature extraction step.
+
+        Returns: ``features_dict`` and ``logits_dict`` output by the algorithm.
+        """
 
     @abc.abstractmethod
     def world_detection(self, test_params: dict, test_data: dict) -> str:
-        pass
+        """
+        Algorithm world detection step.
+
+        Args:
+            test_params: Dict containing test parameters (e.g., input
+                filepaths) for the algorithm feature extraction step.
+            test_data: Dict containing algorithm input/output data (eg,
+                features dict, logits dict).
+
+        Returns: Path to file containing results for world detection.
+        """
 
     @abc.abstractmethod
     def novelty_adaption(self, test_params: dict, test_data: dict) -> None:
-        pass
+        """
+        Algorithm novelty adaption step.
+
+        Args:
+            test_params: Dict containing test parameters (e.g., input
+                filepaths) for the algorithm feature extraction step.
+            test_data: Dict containing algorithm input/output data (eg,
+                features dict, logits dict).
+        """
 
     @abc.abstractmethod
     def novelty_characterization(self, test_params: dict, test_data: dict) -> str:
-        pass
+        """
+        Algorithm novelty characterization step.
+
+        Args:
+            test_params: Dict containing test parameters (e.g., input
+                filepaths) for the algorithm feature extraction step.
+            test_data: Dict containing algorithm input/output data (eg,
+                features dict, logits dict).
+
+        Returns: Path to file containing results for novelty characterization.
+        """
 
     @abc.abstractmethod
     def novelty_classification(self, test_params: dict, test_data: dict) -> str:
-        pass
+        """
+        Algorithm novelty classification step.
+
+        Args:
+            test_params: Dict containing test parameters (e.g., input
+                filepaths) for the algorithm feature extraction step.
+            test_data: Dict containing algorithm input/output data (eg,
+                features dict, logits dict).
+
+        Returns: Path to file containing results for novelty classification.
+        """
